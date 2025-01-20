5b701064b72888fe6929a14c8ad841c622499ec4
Evastrings <elijahakandetea@gmail.com>
2025-01-20 01:00:32 +0000

    setup nativewind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function index() {
+return (
+<View
+style= {{
+flex: 1,
+justifyContent: "center",
+alignmentItems: "center",
+}}
+>
+
+<Link href= "/sign-in">Sign In </Link>
+<Link href= "/explore">Sign In </Link>
+<Link href= "/profile">Sign In </Link>
+<Link href= "properties/1">Sign In </Link>
+</View>
+);
+}
